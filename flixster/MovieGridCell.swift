c17b957dc014a35c6e2760a392eee60deb7e5f21
Christian Pedrigal <pedrigalchristian@gmail.com>
2020-09-10 23:04:00 -0700

    bro

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Christian Pedrigal on 9/9/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
